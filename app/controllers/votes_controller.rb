87e733c121d2b42fb36a731aa139c6558c3ee16a
Speaslee <sophiapeaslee@gmail.com>
2015-11-01 22:57:54 -0500

    added voting system that is limited

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,12 +4,16 @@ class VotesController<ApplicationController
   end
 
   def create
-    Vote.create(
-    user_id: current_user.id,
-    status: params[:vote][:status],
-    votable_id: params[:vote][:votable_id],
-    votable_type: params[:vote][:votable_type]
-    )
-    redirect_to "/posts/1", notice: "vote recorded"
+    if Vote.where(votable_id: params[:vote][:votable_id], user_id: current_user.id).count < 1
+        Vote.create(
+        user_id: current_user.id,
+        status: params[:vote][:status],
+        votable_id: params[:vote][:votable_id],
+        votable_type: params[:vote][:votable_type]
+        )
+        redirect_to :back, notice: "vote recorded"
+    else
+      redirect_to :back, notice: "You already voted for this"
+    end
   end
 end
