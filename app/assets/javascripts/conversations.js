18357da00c00480aaafa544806bb8b91c994eeee
Speaslee <sophiapeaslee@gmail.com>
2015-11-12 14:34:36 -0500

    thing

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,7 +1,7 @@
 //Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(".chosen-select").chosen({
-  no_results_text: "Oops, nothing found!".
+  no_results_text: "Oops, nothing found!",
   disable_search_threshold: 5,
   search_contains: true
 });
